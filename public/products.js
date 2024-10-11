7bbbc5eb138476979b52fe0d676dd66b5ec3df9a
Qu-Ack <134773611+Qu-Ack@users.noreply.github.com>
2024-10-11 15:38:08 +0530

    products can now be partially added into the cart

--- public/products.js
+++ public/products.js
@@ -1,11 +1,33 @@
+const productContainer = document.querySelector(".product_container");
+
 async function getOrders() {
-    try {
-        const response = await fetch("http://localhost:3000/api/product")
-        const data = await response.json()
-        console.log(data)
-    } catch(err) {
-        console.log(err)
-    }
+  try {
+    const response = await fetch("http://localhost:3000/api/product");
+    const data = await response.json();
+    renderProducts(data.products);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-getOrders()
+async function pushCart(productId) {
+  try {
+  } catch (err) {}
+}
+
+function addtoCartListener(addtocartbutton) {
+  addtocartbutton.addEventListener("click", async () => {
+    let productId = addtocartbutton.className.split("-")[1];
+  });
+}
+
+function renderProducts(products) {
+  products.forEach((product) => {
+    productContainer.innerHTML += `<div class="product"><p class="product-name">${product.name}</p><p>${product.price}</p><button class="addtocart-${product.id}">Add To Cart</button></div>`;
+    const addtocart = document.querySelector(`.addtocart-${product.id}`);
+    addtoCartListener(addtocart);
+  });
+}
+
+getOrders();
+
